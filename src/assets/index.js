0493ccfb05ac263c705164360636a57513924df5
DevFaizaan <75069823+DevFaizaan@users.noreply.github.com>
2023-03-31 18:24:16 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -27,7 +27,7 @@ import meta from './company/meta.png';
 import shopify from './company/shopify.png';
 import starbucks from './company/starbucks.png';
 import tesla from './company/tesla.png';
-import QA from './company/qa.png';
+import QA from './company/QA.png';
 import SwipeRight from './company/SwipeRight.png';
 
 import carrent from './carrent.png';
